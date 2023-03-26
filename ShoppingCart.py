745007ecae5ea8d2557e5991f920776c57ef67b4
master <pnayimbila@st.ug.edu.gh>
2023-03-26 02:46:41 +0000

    Craeted an __str__ methos that makes it possible to print an object created from this class.

--- ShoppingCart.py
+++ ShoppingCart.py
@@ -2,3 +2,9 @@
 class ShoppingCart:
     def __init__(self, items=None):
         self.items = []
+
+    #Creates an __str__ method that makes it possible to pass an object created from this class to the print function
+    def __str__(self):
+        return "This is Shooping Cart class"
+    
+    
