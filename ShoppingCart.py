9e8bc3b44857e34ec09b3453c771a4e077d4201a
master <pnayimbila@st.ug.edu.gh>
2023-03-26 02:47:59 +0000

    Added an attribute to the class.

--- ShoppingCart.py
+++ ShoppingCart.py
@@ -1,10 +1,10 @@
 
 class ShoppingCart:
-    def __init__(self, items=None):
+    def __init__(self, items=None, total_price=None):
         self.items = []
+        self.total_price = total_price
 
     #Creates an __str__ method that makes it possible to pass an object created from this class to the print function
     def __str__(self):
         return "This is Shooping Cart class"
-    
     
